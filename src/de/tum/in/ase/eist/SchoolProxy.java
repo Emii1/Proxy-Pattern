3ee13285c345d8f8585685ee81b568f88e3030ac
Emi <emi.mano@tum.de>
2022-06-12 00:29:20 +0200

    ab

--- src/de/tum/in/ase/eist/SchoolProxy.java
+++ src/de/tum/in/ase/eist/SchoolProxy.java
@@ -4,17 +4,16 @@ import java.net.URL;
 import java.util.Set;
 
 public class SchoolProxy implements ConnectionInterface{
-    @Override
+
     public void connect(URL url) {
 
     }
 
-    @Override
+
     public void disconnect() {
 
     }
 
-    @Override
     public boolean isConnected() {
         return false;
     }
